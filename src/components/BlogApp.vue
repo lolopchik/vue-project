8b20c6742bd052fa82fbee27a9c19d18ad2cd71a
lolopchik <bombov2009@gmail.com>
2024-02-25 17:11:37 +0400

    Important fix

---
+++ src/components/BlogApp.vue
@@ -0,0 +1,84 @@
+<script>
+import BlogPost from './BlogPost.vue';
+export default {
+  components: {
+    BlogPost
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'Как сделать lorum ipsum?',
+          text: 'Для lorum ipsum нужно: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, earum consequatur debitis perferendis blanditiis quod deleniti ea officiis laudantium. Iste quae veritatis, nam atque iusto voluptatem asperiores libero fugiat incidunt.',
+          author: 'Лорум Ипсум',
+          date: '13.03.2024',
+          comments: [
+            'Круто!!!',
+            'Недооценённый материал...',
+            'Лучшее, что я читал.'
+          ],
+          image: 'post1'
+        },
+        {
+          title: 'Как сделать lorum ipsum?',
+          text: 'Для lorum ipsum нужно: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, earum consequatur debitis perferendis blanditiis quod deleniti ea officiis laudantium. Iste quae veritatis, nam atque iusto voluptatem asperiores libero fugiat incidunt.',
+          author: 'Лорум Ипсум',
+          date: '13.03.2024',
+          comments: [
+            'Круто!!!',
+            'Недооценённый материал...',
+            'Лучшее, что я читал.'
+          ],
+          image: 'post1'
+        },
+        {
+          title: 'Как сделать lorum ipsum?',
+          text: 'Для lorum ipsum нужно: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, earum consequatur debitis perferendis blanditiis quod deleniti ea officiis laudantium. Iste quae veritatis, nam atque iusto voluptatem asperiores libero fugiat incidunt.',
+          author: 'Лорум Ипсум',
+          date: '13.03.2024',
+          comments: [
+            'Круто!!!',
+            'Недооценённый материал...',
+            'Лучшее, что я читал.'
+          ],
+          image: 'post1'
+        },
+      ],
+      fontSize: 16
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<template>
+  <header class="mb-5">
+    <nav class="navbar">
+      <div class="container-fluid">
+        <h1 class="navbar-brand text-white">Лучший блог на планете</h1>
+      </div>
+    </nav>
+  </header>
+  <main class="container">
+    <h2>Посты:</h2>
+    <div class="row g-2">
+      <!-- Вывод всех постов -->
+      <div class="col-12">
+        <!-- Тут должен быть пост -->
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+.navbar {
+  background-color: #008cd5;
+  color: white;
+}
+</style>
---
+++ src/components/BlogPostTemplate.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<template>
+    <div class="card shadow-sm">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img"
+            aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+                dy=".3em">Thumbnail</text>
+        </svg>
+        <div class="card-body">
+            <p class="card-subtitle text-secondary">Автор поста</p>
+            <h3 class="card-title">Заголовок поста</h3>
+            <p class="card-text">Текст поста</p>
+            <div class="d-flex justify-content-between align-items-center ">
+                <div class="btn-group">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">-</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">+</button>
+                    </div>
+                    <!-- <input v-model="fontSizeNew" class="form-control ms-3" type="text" placeholder="px" style="width: 50px;">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Установить</button> -->
+
+                </div>
+                <small class="text-body-secondary">Дата поста</small>
+            </div>
+
+            <hr>
+            <h4>Комментарии:</h4>
+            <ul class="list-group list-group-flush">
+                <!-- Вывод всех комментарии -->
+                <li class="list-group-item" >Аноним: Я аноним</li>
+            </ul>
+
+        </div>
+    </div>
+</template>
+<style>
+</style>
